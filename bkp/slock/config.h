d12802095d0510bde9ec4cfb35fe568d5719bcb5
Saatwat Sakhaa <105187044+nirodhvana@users.noreply.github.com>
2025-04-24 20:41:27 +0545

    Under reconstruction

--- bkp/slock/config.h
+++ bkp/slock/config.h
@@ -1,11 +1,12 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "Saatwat";
+static const char *group = "Sakhaa";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[FAILED] = "#96291f",   /* wrong password */
+
 };
 
 /* treat a cleared input like a wrong password (color) */
